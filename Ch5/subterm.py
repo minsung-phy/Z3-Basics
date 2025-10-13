bd345b1d3f9c33575f0ac216dbcdeaf535c7c79b
minsung-phy <minsung.phy@gamil.com>
2025-10-13 21:31:01 +0900

    dev : Subterm Simplification

---
+++ Ch5/subterm.py
@@ -0,0 +1,31 @@
+def subterms(t):
+    seen = {}
+    def subterms_rec(t):
+        if is_app(t):
+            for ch in t.children():
+                if ch in seen:
+                    continue
+                seen[ch] = True
+                yield ch
+                yield from subterms_rec(ch)
+    return { s for s in subterms_rec(t) }
+
+def are_equal(s, t1, t2):
+    s.push()
+    s.add(t1 != t2)
+    r = s.check()
+    s.pop()
+    return r == unsat
+
+def simplify(slv, mdl, t):
+    subs = subterms(t)
+    values = { s: mdl.eval(s) for s in subs }
+    values[t] = mdl.eval(t)
+    def simplify_rec(t):
+        subs = subterms(t)
+        for s in subs:
+            if s.sort().eq(t.sort()) and values[s].eq(values[t]) and are_equal(slv, s, t):
+                return simplify_rec(s)
+        chs = [simplify_rec(ch) for ch in t.children()]
+        return t.decl()(chs)
+    return simplify_rec(t)
